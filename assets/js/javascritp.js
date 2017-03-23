051969be1ecee3af7851b848dc8c8e3084ed326e
Shirley Silvana Suarez Startary <Shirley Silvana Suarez Startary>
2017-03-23 16:53:22 -0600

    primer upload de la calculadora

---
+++ assets/js/javascritp.js
@@ -0,0 +1,102 @@
+var arreglo=[];
+//var guardarNumeros =[];
+
+// var x=mostrarNumero;
+function mostrarNumero(id){
+  var numero = document.getElementById(id).value;
+  console.log(numero);
+  var caja = document.getElementById("caja").value+=numero;
+  //caja.value+=numero;
+  return caja;
+}
+
+function suma(){
+    //var operador = document.getElementById('suma');
+
+    var valorCajita = document.getElementById('caja').value;
+    var suma = 0;
+    if(arreglo[0] == undefined){
+        arreglo[0] = valorCajita;
+        console.log(arreglo);
+    }else{
+        arreglo[1] = valorCajita;
+        console.log(arreglo);
+        suma = parseInt(arreglo[0]) + parseInt(arreglo[1]);
+        arreglo[0] = suma;
+        console.log(suma);
+        // arreglo=[];
+
+    }
+    document.getElementById('caja').value = '';
+    document.getElementById("caja").value = suma;
+
+}
+
+
+function producto(){
+    //var operador = document.getElementById('suma');
+
+    var valorCajita = document.getElementById('caja').value;
+    var producto = 1;
+    if(arreglo[0] == undefined){
+        arreglo[0] = valorCajita;
+        console.log(arreglo);
+    }else{
+        arreglo[1] = valorCajita;
+        console.log(arreglo);
+        producto = parseInt(arreglo[0]) * parseInt(arreglo[1]);
+        arreglo[0] = producto;
+        console.log(producto);
+        // arreglo=[];
+
+
+    }
+    document.getElementById('caja').value = '';
+
+}
+
+function division(){
+    //var operador = document.getElementById('suma');
+
+    var valorCajita = document.getElementById('caja').value;
+    var div = 1;
+    if(arreglo[0] == undefined){
+        arreglo[0] = valorCajita;
+        console.log(arreglo);
+    }else{
+        arreglo[1] = valorCajita;
+        console.log(arreglo);
+        div = parseInt(arreglo[0]) / parseInt(arreglo[1]);
+        arreglo[0] = div;
+        console.log(div);
+        // arreglo=[];
+
+
+    }
+    document.getElementById('caja').value = '';
+    document.getElementById('caja')=div;
+
+}
+
+function restar(){
+    //var operador = document.getElementById('suma');
+
+    var valorCajita = document.getElementById('caja').value;
+    var resta= 1;
+    if(arreglo[0] == undefined){
+        arreglo[0] = valorCajita;
+        console.log(arreglo);
+    }else{
+        arreglo[1] = valorCajita;
+        console.log(arreglo);
+        resta = parseInt(arreglo[0]) * parseInt(arreglo[1]);
+        arreglo[0] = resta;
+        console.log(resta);
+        // arreglo=[];
+
+
+    }
+    document.getElementById('caja').value = '';
+    document.getElementById('caja').value = resta;
+
+}
